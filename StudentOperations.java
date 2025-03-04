6f8744a875b376baf6e62b8e02b11a64e0ce991e
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-04 23:24:19 +0530

    addStudent funciot added

--- StudentOperations.java
+++ StudentOperations.java
@@ -8,6 +8,13 @@ class StudentOperations {
     public StudentOperations() {
         this.students = new ArrayList<>();
     }
+    
+     // Method to add a student to the list and sort by PRN
+     public void addStudent(Student student) {
+        students.add(student);
+        students.sort(Comparator.comparingLong(Student::getPRN)); // Sorting by PRN
+        System.out.println("Student added successfully.");
+    }
 
     
    
