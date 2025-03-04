f8f44008cb47fb46c788d6266490230fe8f0f36a
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-04 23:00:42 +0530

    added

--- Main.java
+++ Main.java
@@ -2,11 +2,15 @@
 //PRN:23070126088
 //AIML B1
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        int choice;
     }
     
 }
