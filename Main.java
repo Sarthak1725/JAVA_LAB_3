cb75b5d2a82d195b1dee305c1adff2d6a424ce48
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-04 22:54:51 +0530

    Input

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@
 //PRN:23070126088
 //AIML B1
 
-import java.text.*;
 import java.util.*;
 
-public class Main {
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
     
 }
