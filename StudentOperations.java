50a2fbeac8ee6dc21be3eefe904984e4ab456196
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-04 23:28:48 +0530

    Search by name

--- StudentOperations.java
+++ StudentOperations.java
@@ -36,6 +36,16 @@ class StudentOperations {
         return null;
     }
 
+      // Method to search student by Name
+      public Student searchByName(String name) {
+        for (Student student : students) {
+            if (student.getName().equalsIgnoreCase(name)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
     
    
 
